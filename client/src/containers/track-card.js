aa109545759b154db021e0008cc20ea9132dd7b2
Charlie Nader <nitro.naj@gmail.com>
2023-04-27 14:48:05 +0000

    add resolver and get argument for query

--- client/src/containers/track-card.js
+++ client/src/containers/track-card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 import { colors, mq } from '../styles';
 import { humanReadableTimeFromSeconds } from '../utils/helpers';
 
@@ -8,10 +9,10 @@ import { humanReadableTimeFromSeconds } from '../utils/helpers';
  * for each track populating the tracks grid homepage.
  */
 const TrackCard = ({ track }) => {
-  const { title, thumbnail, author, length, modulesCount } = track;
+  const { id, title, thumbnail, author, length, modulesCount } = track;
 
   return (
-    <CardContainer>
+    <CardContainer to={`track/${id}`}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail} alt={title} />
@@ -36,7 +37,7 @@ const TrackCard = ({ track }) => {
 export default TrackCard;
 
 /** Track Card styled components */
-const CardContainer = styled('div')({
+const CardContainer = styled(Link)({
   borderRadius: 6,
   color: colors.text,
   backgroundSize: 'cover',
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -4,12 +4,21 @@ const resolvers = {
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
+    // get single track by ID, for track page
+    track: (_, {id}, {dataSources}) => {
+      return dataSources.trackAPI.getTrack(id);
+    }
   },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id)
+    }
   },
+
+
 };
 
 module.exports = resolvers;
